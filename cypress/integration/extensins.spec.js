3a242423da73e28ea16609c8df5cca1fbb513f1b
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-07-02 10:52:04 -0300

    gitignore

---
+++ cypress/integration/extensins.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('', () => {
+  it('', () => {
+    cy.request({
+      method: '',
+      url: '',
+      headers: { 'Accept-Language': 'en-us', },
+    });
+    cy.clickAlert()
+  });
+});
